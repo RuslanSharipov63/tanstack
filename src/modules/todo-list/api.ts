af024958c2dd410e7ba82dbb4cccea62408ab5b2
unknown <sharipov.r@mail.ru>
2025-03-31 21:07:58 +0400

    first commit

---
+++ src/modules/todo-list/api.ts
@@ -0,0 +1,27 @@
+const BASE_URL = 'http://localhost:3000';
+
+export type PaginatedResult<T> = {
+        first: number,
+        prev: number | null,
+        next: number | null,
+        last: number,
+        pages: number,
+        items: number,
+        data: T[],  
+} 
+
+export type TodoDto = {
+    id: string;
+    text: string;
+    done: boolean;
+  };
+
+export const todoListApi = {
+    getTodoList: (
+        {page}:{page: number}, 
+        {signal}: {signal: AbortSignal}) => {
+        return fetch(`${BASE_URL}/tasks?_page=${page}&_per_page=10`, {
+            signal
+        }).then(res => res.json() as Promise <PaginatedResult<TodoDto>>);
+    }
+}
---
+++ src/modules/todo-list/todo-list.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import { todoListApi } from "./api";
+import { useState } from "react";
+
+export function TodoList() {
+  const [page, setPage] = useState(1);
+
+  const { data: todoItems, error, isPending } = useQuery({
+    queryKey: ["tasks", "list"],
+    queryFn: meta => todoListApi.getTodoList({ page }, meta)
+  });
+
+  if (isPending) {
+    return <div>Загрузка...</div>;
+  }
+
+  if (error) {
+    return <div>error: {JSON.stringify(error)}</div>;
+  }
+
+  return (
+    <div className="p-5 mx-auto max-w-[1200px] mt-10">
+      <h1 className="text-3xl font-bold underline mb-5">TodoList</h1>
+      <div className="flex flex-col gap-4">
+        {todoItems.data.map(item => (
+          <div className="border border-slate-300 rounded p-3" key={item.id}>
+            {item.text}
+          </div>
+        ))}
+      </div>
+      <div className="flex gap-2 mt-4">
+        <button
+          onClick={() => setPage(p => Math.max(p - 1, 0))}
+          className="p-3 rounded border border-teal-500"
+        >
+          prev
+        </button>
+        <button
+          onClick={() => setPage(p => Math.max(p + 1, todoItems.pages))}
+          className="p-3 rounded border border-teal-500"
+        >
+          next
+        </button>
+      </div>
+    </div>
+  );
+}
