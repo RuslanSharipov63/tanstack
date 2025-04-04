f43adb92fb6479ba2d00ad3d3e392fece25f812e
RuslanSharipov63 <sharipov.r@mail.ru>
2025-04-04 15:57:50 +0400

    infinity

--- src/modules/todo-list/api.ts
+++ src/modules/todo-list/api.ts
@@ -1,27 +1,47 @@
+import { infiniteQueryOptions, queryOptions } from "@tanstack/react-query";
+
 const BASE_URL = 'http://localhost:3000';
 
 export type PaginatedResult<T> = {
-        first: number,
-        prev: number | null,
-        next: number | null,
-        last: number,
-        pages: number,
-        items: number,
-        data: T[],  
-} 
+    first: number,
+    prev: number | null,
+    next: number | null,
+    last: number,
+    pages: number,
+    items: number,
+    data: T[],
+}
 
 export type TodoDto = {
     id: string;
     text: string;
     done: boolean;
-  };
+};
 
 export const todoListApi = {
     getTodoList: (
-        {page}:{page: number}, 
-        {signal}: {signal: AbortSignal}) => {
+        { page }: { page: number },
+        { signal }: { signal: AbortSignal }) => {
         return fetch(`${BASE_URL}/tasks?_page=${page}&_per_page=10`, {
             signal
-        }).then(res => res.json() as Promise <PaginatedResult<TodoDto>>);
+        }).then(res => res.json() as Promise<PaginatedResult<TodoDto>>);
+    },
+
+
+    getTodoListQueryOptions: ({ page }: { page: number }) => {
+        return queryOptions({
+            queryKey: ["tasks", "list", { page }],
+            queryFn: meta => todoListApi.getTodoList({ page }, meta),
+        })
+    },
+
+    getTodoListInfinityQueryOptions: () => {
+        return infiniteQueryOptions({
+            queryKey: ["tasks", "list"],
+            queryFn: meta => todoListApi.getTodoList({ page: meta.pageParam }, meta),
+            initialPageParam: 1,
+            getNextPageParam: result => result.next,
+            select: result => result.pages.flatMap(page => page.data)
+        })
     }
 }
--- src/modules/todo-list/todo-list.tsx
+++ src/modules/todo-list/todo-list.tsx
@@ -1,17 +1,31 @@
 "use client";
-import { useQuery } from "@tanstack/react-query";
+import {
+  useInfiniteQuery
+} from "@tanstack/react-query";
 import { todoListApi } from "./api";
-import { useState } from "react";
+import { useCallback, useState, useRef } from "react";
 
 export function TodoList() {
-  const [page, setPage] = useState(1);
+  const [enabled, setEnable] = useState(false);
 
-  const { data: todoItems, error, isPending } = useQuery({
-    queryKey: ["tasks", "list", {page}],
-    queryFn: meta => todoListApi.getTodoList({ page }, meta)
+  const {
+    data: todoItems,
+    error,
+    isLoading,
+    isPlaceholderData,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage
+  } = useInfiniteQuery({  
+    ...todoListApi.getTodoListInfinityQueryOptions(),
+    enabled: enabled,
   });
 
-  if (isPending) {
+  const cursorRef = useIntersection(() => {
+    fetchNextPage();
+  })
+
+  if (isLoading) {
     return <div>Загрузка...</div>;
   }
 
@@ -19,30 +33,49 @@ export function TodoList() {
     return <div>error: {JSON.stringify(error)}</div>;
   }
 
+  
+
   return (
     <div className="p-5 mx-auto max-w-[1200px] mt-10">
       <h1 className="text-3xl font-bold underline mb-5">TodoList</h1>
-      <div className="flex flex-col gap-4">
-        {todoItems.data.map(item => (
+      <button onClick={() => setEnable(e => !e)}>Toggle enabled</button>
+      <div
+        className={
+          "flex flex-col gap-4" + (isPlaceholderData ? " opacity-50" : "")
+        }
+      >
+        {todoItems?.map(item => (
           <div className="border border-slate-300 rounded p-3" key={item.id}>
             {item.text}
           </div>
         ))}
       </div>
-      <div className="flex gap-2 mt-4">
-        <button
-          onClick={() => setPage(p => Math.max(p - 1, 0))}
-          className="p-3 rounded border border-teal-500"
-        >
-          prev
-        </button>
-        <button
-          onClick={() => setPage(p => Math.min(p + 1, todoItems.pages))}
-          className="p-3 rounded border border-teal-500"
-        >
-          next
-        </button>
+      <div className="flex gap-2 mt-4" ref={cursorRef}>
+        {!hasNextPage && <div>Нет данных для загрузки</div>}
+        {isFetchingNextPage && <div>...Loading</div>}
       </div>
     </div>
   );
+}
+
+export function useIntersection(onIntersect: () => void) {
+
+const unsubscribe = useRef(() => {})
+
+  /* этот хук вызывается когда какой-нибдуь попадает в видимую часть экрана. это нужно для подгрузки */
+  return useCallback((el: HTMLDivElement | null) => {
+  const observer = new IntersectionObserver(entries => {
+      entries.forEach(intersection => {
+        if (intersection.isIntersecting) {
+          onIntersect();
+        }
+      });
+    });
+    if(el) {
+      observer.observe(el);
+      unsubscribe.current = () => observer.disconnect();
+    } else {
+      unsubscribe.current();
+    }
+  }, []);
 }
